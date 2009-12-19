b0d1c55a24dc50663ea2284f5c926cd6229d5a18
Carl Ritson <critson@perlfu.co.uk>
2009-12-19 16:50:09 +0900

    Work on loading a TBC from USB. This crashes somewhere in the TBC decoder.

--- tvm.c
+++ tvm.c
@@ -30,71 +30,90 @@ void main (void) {
 		int running 	= 1;
 
 		nx_display_clear ();
-		nx_display_string ("I am the Transterpreter,");
+		nx_display_string ("I am the TVM.");
 		nx_display_end_line ();
-		nx_display_string ("        give me all your bytecodes.");
-		nx_display_end_line ();
-		
+
 		nx_usb_read (buffer, NX_USB_PACKET_SIZE);
 		while (usb == 0) {
 			if ((pos = nx_usb_data_read ()) >= 8) {
-				if ((usb = valid_tbc_header (buffer))) {
-					tbc = (BYTE *) mem_pool;
-					tbc_length = pos + usb;
+				if ((tbc_length = valid_tbc_header (buffer))) {
+					tbc_length 	+= 8;
+					tbc 		= (BYTE *) mem_pool;
 					for (i = 0; i < pos; ++i) {
 						tbc[i] = buffer[i];
 					}
+					usb = tbc_length - pos;
 				}
 			} else {
 				nx_systick_wait_ms (200);
 			}
 		}
 
-		nx_display_string ("Got TEncode header.");
+		nx_display_string ("Got header (");
+		nx_display_uint (tbc_length);
+		nx_display_string (")");
 		nx_display_end_line ();
+		
+		nx_display_cursor_set_pos (0, 2);
 		nx_display_uint (usb);
-		nx_display_string (" bytes remaining");
-		nx_display_end_line ();
+		nx_display_string ("      ");
 
 		while (usb > 0) {
 			U32 tmp;
 
 			if (usb >= NX_USB_PACKET_SIZE) {
-				nx_usb_read (buffer, NX_USB_PACKET_SIZE);
+				nx_usb_read (&(tbc[pos]), NX_USB_PACKET_SIZE);
 			} else {
-				nx_usb_read (buffer, usb);
+				nx_usb_read (&(tbc[pos]), usb);
 			}
 
-			while (!(tmp = nx_usb_data_read ())) {
-				nx_display_uint (usb);
-				nx_display_string (" bytes remaining");
-				nx_display_end_line ();
-				nx_systick_wait_ms (200);
-			}
+			while (!(tmp = nx_usb_data_read ()))
+				continue;
 
-			for (i = 0; i < tmp; ++i) {
-				tbc[pos++] = buffer[i];
-				usb--;
-			}
+			pos += tmp;
+			usb -= tmp;
+			
+			nx_display_cursor_set_pos (0, 2);
+			nx_display_uint (usb);
+			nx_display_string ("      ");
 		}
+		
+		nx_display_cursor_set_pos (0, 2);
+		nx_display_string ("Got TBC.    ");
+		nx_display_end_line ();
+		nx_systick_wait_ms (200);
 
-		tlsf_init_memory_pool (NX_USERSPACE_SIZE, 
+		tlsf_init_memory_pool (NX_USERSPACE_SIZE - tbc_length, 
 				(void *)(((U8 *) mem_pool) + tbc_length));
-
+		
 		tvm_ectx_init (&tvm, &context);
 		context.mem_pool = mem_pool;
 		context.get_time = nxt_get_time;
 		context.modify_sync_flags = nxt_modify_sync_flags;
 		//context.sffi_table = sffi_table;
 		//context.sffi_table_length = sffi_table_length;
-
+		
+		nx_display_string ("Loading TBC...");
+		nx_display_end_line ();
+		nx_systick_wait_ms (1000);
+		
 		if (load_context_with_tbc (&context, tbc, tbc_length) != 0) {
-			/* FIXME: display program load failed */
+			nx_display_string ("Unable to decode TBC!");
+			nx_systick_wait_ms (3000);
 			continue;
 		}
 
+		nx_display_string ("Running...");
+		nx_display_end_line ();
+		nx_systick_wait_ms (1000);
+		
 		while (running) {
 			int ret = tvm_run (&context);
+			
+			nx_display_uint (ret);
+			nx_display_end_line ();
+			nx_systick_wait_ms (1000);
+			
 			switch (ret) {
 				case ECTX_PREEMPT:
 				case ECTX_TIME_SLICE: {
@@ -129,6 +148,11 @@ void main (void) {
 					running = 0;
 				}
 			}
+			
+			nx_display_end_line ();
+			nx_display_string ("Finished       ");
+			nx_display_end_line ();
+			nx_systick_wait_ms (3000);
 		}
 	}
 
